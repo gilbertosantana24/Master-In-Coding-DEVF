48b998e5a507648d7eedc077b36f7e1d7d65a5aa
gilbertosantana24 <gilbertobuja@gmail.com>
2021-10-26 17:48:14 -0700

    added home

---
+++ 05.React/14.songlist-context/src/views/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+return (
+
+    <div className="home-container">
+        <div className="izquierdo">
+            <h2>lado izquierdo</h2>
+            
+        </div>
+
+        <div className="derecho">
+            <h2>lado derecho</h2>
+        </div>
+    </div>
+    )
+}
+
+export default Home;
